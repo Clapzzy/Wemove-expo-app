af04e3b5f05cd57070909cd9430e033f8f7a61b1
Clapzzy <martin.kurtew@gmail.com>
2024-05-18 09:33:00 +0300

    a

--- src/app/(main)/search.tsx
+++ src/app/(main)/search.tsx
@@ -12,20 +12,30 @@ export default function Search() {
     console.log(search)
   }, [search])
 
+  function removeKeyboard() {
+    Keyboard.dismiss()
+  }
+
   return (
     <View className="items-center bg-[#262626] flex-1 color-white">
       <ImageBackground className="w-full h-full" source={require('assets/waves2.png')}>
         <SafeAreaView >
-          <View className="px-4"  >
-            <CustomText text="Search" type="ExtraBold" className="text-[#BFE500] text-32" />
-            <TextInput className="bg-[#080807] opacity-70 h-10 w-fit mt-2 rounded-lg  pl-3" onChangeText={setSearch} >
-              <Image source={require("../../../assets/search.png")} />
-              {search.length == 0 ? (<CustomText text="Search" type="Regular" className="text-20 text-[#A3A3A3]" />) : (<CustomText text={search} type="Regular" className="text-20 text-[#A3A3A3]" />)}
-            </TextInput>
-            <FixedButton text="Хора" backgroundColorButton="#030302B3" textColor="#A3A3A3" />
-            <FixedButton text="Групи" backgroundColorButton="#030302B3" textColor="#2C2C2C" />
-            <View className="h-[1] w-fit bg-[#BFE500]"></View>
-          </View>
+          <TouchableWithoutFeedback onPress={removeKeyboard}>
+            <View className="px-4"  >
+              <CustomText text="Search" type="ExtraBold" className="text-[#BFE500] text-32" />
+              <TextInput className="bg-[#080807] opacity-70 text-20 text-[#A3A3A3] h-10 w-fit mt-2 rounded-lg  pl-3" onChangeText={setSearch}
+                placeholder="Search" // Clear placeholder text when typing
+                placeholderTextColor="#A3A3A3"
+                value={search}
+              >
+              </TextInput>
+              <View className="flex-row justify-between mt-5 mb-3">
+                <FixedButton text="Хора" backgroundColorButton="#030302B3" textColor="#A3A3A3" />
+                <FixedButton text="Групи" backgroundColorButton="#030302B3" textColor="#2C2C2C" />
+              </View>
+              <View className="h-[1] w-fit bg-[#BFE500]"></View>
+            </View>
+          </TouchableWithoutFeedback>
         </SafeAreaView>
       </ImageBackground>
     </View >
