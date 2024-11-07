8d4e13ce1ae7f61c2da3e3cd10dc18838e30f875
Clapzzy <martin.kurtew@gmail.com>
2024-11-07 23:39:11 +0200

    ,

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,18 +2,21 @@ import "../global.css";
 import { Slot, Stack } from "expo-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import 'react-native-reanimated'
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 const client = new QueryClient()
 
 
 export default function Layout() {
   return (
-    <QueryClientProvider client={client}>
-      <Stack screenOptions={{ headerShown: false, orientation: "portrait_up" }}>
-        <Stack.Screen name="(main)" options={{ headerShown: false }} />
-        <Stack.Screen name="(profile-maker)" options={{ headerShown: false }} />
-        <Stack.Screen name="(test)" options={{ headerShown: false }} />
-      </Stack>
-    </QueryClientProvider>
+    <GestureHandlerRootView>
+      <QueryClientProvider client={client}>
+        <Stack screenOptions={{ headerShown: false, orientation: "portrait_up" }}>
+          <Stack.Screen name="(main)" options={{ headerShown: false }} />
+          <Stack.Screen name="(profile-maker)" options={{ headerShown: false }} />
+          <Stack.Screen name="(test)" options={{ headerShown: false }} />
+        </Stack>
+      </QueryClientProvider>
+    </GestureHandlerRootView>
   );
 }
